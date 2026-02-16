e6083f91b413c3907fb0662d47750ac952c4637c
Aeshamodi7714 <aeshamodi7714@gmail.com>
2026-02-16 12:14:35 +0530

    dom-file

--- java/dom.js
+++ java/dom.js
@@ -65,12 +65,57 @@ e.textContent = "h1 tag"
 // ----------------------------------------------------
 // Attribute Manipulation
 // getAttribute, setAttribute, removeAttribute
-let link = document.querySelector('a')
-console.dir(link)
-// console.log("get Attribute", info) // -- give value of href attribute
+let link = document.querySelector("a");
+console.dir(link);
+console.log(link.href);
+console.log("href value", link.getAttribute("href"))
 
-// link.href = "https://www.google.com/"
-// element.setAttribute("name", "change")
-link.setAttribute("href", "www.google.com")
-let info = link.getAttribute("href");
-console.log("get Attribute", info) // -- give value
+// setAttribute
+link.setAttribute("href", "www.google.com");
+console.log("href value", link.getAttribute("href"))
+
+// removeAttribute
+link.removeAttribute("href");
+
+// ----------------------------------------------------
+// Dynamic DOM manipulation
+// createElement, appendChild, removeChild, prepend
+// createElement
+let text_h1 = document.createElement("h1")
+console.log(text_h1);
+text_h1.textContent = "create h1 with JS"
+
+// append / prepend
+document.body.prepend(text_h1);
+
+// appendChild
+let outer = document.createElement("div")
+outer.classList.add("outer");
+document.body.append(outer);
+
+let inner = document.createElement("div");
+inner.classList.add("inner");
+outer.appendChild(inner)
+
+
+// append vs appendChild
+// remove vs removeChild
+
+// ----------------------------------------------------
+// Style Updates via .style and classList (add, remove, toggle)
+console.dir(text_h1)
+text_h1.style.backgroundColor = "Black";
+text_h1.style.color = "white";
+text_h1.style.padding = "10px";
+
+// ClassList -- add
+let card = document.querySelector(".card")
+console.dir(card)
+card.classList.add("card-style");
+
+// remove
+card.classList.remove("card")
+
+// toggle -- class --> available --> remove it
+// Class--> not available -->add it
+card.classList.toggle("card")
